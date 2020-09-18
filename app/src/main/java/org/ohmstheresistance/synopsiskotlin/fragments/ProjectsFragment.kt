e94f696daeb5229d1f64cea847d02843bcc401af
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-09-18 01:29:38 -0400

    Added Adapter for Projects ViewPager. EssentialFactsFragment added to viewpager

--- app/src/main/java/org/ohmstheresistance/synopsiskotlin/fragments/ProjectsFragment.kt
+++ app/src/main/java/org/ohmstheresistance/synopsiskotlin/fragments/ProjectsFragment.kt
@@ -1,28 +1,46 @@
 package org.ohmstheresistance.synopsiskotlin.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import org.ohmstheresistance.synopsiskotlin.R
-
+import org.ohmstheresistance.synopsiskotlin.adapters.ProjectsViewPagerAdapter
+import org.ohmstheresistance.synopsiskotlin.databinding.ProjectsFragmentBinding
 
 class ProjectsFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.projects_fragment, container, false)
+
+        val projectsBinding = DataBindingUtil.inflate<ProjectsFragmentBinding>(inflater, R.layout.projects_fragment, container, false)
+
+        setUpViewPager(projectsBinding.projectsViewPager)
+        projectsBinding.tabs.setupWithViewPager(projectsBinding.projectsViewPager)
+
+        return projectsBinding.root
+    }
+
+    private fun setUpViewPager(viewPager: ViewPager?) {
+        val projectsViewPagerAdapter = activity?.getSupportFragmentManager()?.let {
+            ProjectsViewPagerAdapter(
+                it
+            )
+        }
+        projectsViewPagerAdapter?.addFragments(EssentialFactsFragment(), "Essential Facts")
+//        projectsViewPagerAdapter.addFragments(MastermindFragment(), "Mastermind")
+//        projectsViewPagerAdapter.addFragments(KnowYourWorldFragment(), "Know Your World")
+//        projectsViewPagerAdapter.addFragments(AndroidTriviaFragment(), "Android Trivia")
+//        projectsViewPagerAdapter.addFragments(CupidShuffleFragment(), "Cupid Shuffle")
+//        projectsViewPagerAdapter.addFragments(CeeLoFragment(), "CeeLo")
+//        projectsViewPagerAdapter.addFragments(TributeFragment(), "Tribute")
+        viewPager?.adapter = projectsViewPagerAdapter
+        //viewPager.setPageTransformer(true, CubeOutTransformer())
     }
 
     }
